8fc675bcc9c8f72f9ab224942be36adaa901aaf5
Spencer Gibb <spencer@gibb.us>
2019-09-11 15:33:20 -0400

    Updates to use new gateway rsocket client module

--- ping/src/main/java/org/springframework/cloud/rsocket/sample/ping/PingApplication.java
+++ ping/src/main/java/org/springframework/cloud/rsocket/sample/ping/PingApplication.java
@@ -3,29 +3,17 @@ package org.springframework.cloud.rsocket.sample.ping;
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tag;
-import io.rsocket.micrometer.MicrometerRSocketInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
-import org.springframework.cloud.gateway.rsocket.autoconfigure.GatewayRSocketAutoConfiguration;
-import org.springframework.cloud.gateway.rsocket.support.Forwarding;
-import org.springframework.cloud.gateway.rsocket.support.RouteSetup;
-import org.springframework.cloud.gateway.rsocket.support.TagsMetadata;
+import org.springframework.cloud.gateway.rsocket.client.BrokerClient;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.RSocketStrategies;
-
-import static org.springframework.cloud.gateway.rsocket.support.WellKnownKey.INSTANCE_NAME;
-import static org.springframework.cloud.gateway.rsocket.support.WellKnownKey.SERVICE_NAME;
 
 @SpringBootApplication
 public class PingApplication {
@@ -35,36 +23,19 @@ public class PingApplication {
 	}
 
 	@Bean
-	//TODO: client module?
-	public RSocketStrategiesCustomizer gatewayRSocketStrategiesCustomizer() {
-		return strategies -> {
-			strategies.decoder(new Forwarding.Decoder(), new RouteSetup.Decoder())
-					.encoder(new Forwarding.Encoder(), new RouteSetup.Encoder());
-		};
-	}
-
-	@Bean
-	public Ping ping(Environment env, MeterRegistry meterRegistry, RSocketRequester.Builder requesterBuilder, RSocketStrategies strategies) {
-		//TODO: client module
-		GatewayRSocketAutoConfiguration.registerMimeTypes(strategies);
-		return new Ping(env, meterRegistry, requesterBuilder);
+	public Ping ping(BrokerClient brokerClient) {
+		return new Ping(brokerClient);
 	}
 
 	@Slf4j
 	public static class Ping implements ApplicationListener<ApplicationReadyEvent> {
 
-		private MeterRegistry meterRegistry;
-		private final RSocketRequester.Builder requesterBuilder;
-
-		private final String id;
+		private final BrokerClient client;
 
 		private final AtomicInteger pongsReceived = new AtomicInteger();
-		private Flux<String> pongFlux;
 
-		public Ping(Environment env, MeterRegistry meterRegistry, RSocketRequester.Builder requesterBuilder) {
-			this.id = env.getProperty("ping.id", "1");
-			this.meterRegistry = meterRegistry;
-			this.requesterBuilder = requesterBuilder;
+		public Ping(BrokerClient client) {
+			this.client = client;
 		}
 
 		@Override
@@ -72,46 +43,38 @@ public class PingApplication {
 			ConfigurableEnvironment env = event.getApplicationContext().getEnvironment();
 
 			String requestType = env.getProperty("ping.request-type", "request-channel");
-			log.info("Starting Ping"+id+" request type: " + requestType);
+			log.info("Starting Ping" + client.getProperties().getRouteId() + " request type: " + requestType);
 
-			Integer serverPort = env.getProperty("spring.rsocket.server.port",
-					Integer.class, 7002);
+			RSocketRequester requester = client.connect().block();
 
-			MicrometerRSocketInterceptor interceptor = new MicrometerRSocketInterceptor(meterRegistry, Tag
-					.of("component", "ping"));
-
-			TagsMetadata tagsMetadata = TagsMetadata.builder()
-					.with(INSTANCE_NAME, "ping" + id)
-					.build();
-			RouteSetup routeSetup = new RouteSetup(new Long(id), "ping", tagsMetadata.getTags());
-			RSocketRequester requester = requesterBuilder
-					.setupMetadata(routeSetup, RouteSetup.ROUTE_SETUP_MIME_TYPE)
-					.connectTcp("localhost", serverPort)
-					.block();
-
-			TagsMetadata forwardinMetadata = TagsMetadata.builder()
-					.with(SERVICE_NAME, "pong")
-					.build();
-			Forwarding forwarding = new Forwarding(new Long(id), forwardinMetadata.getTags());
-
-			if (requestType.equals("request-response")) {
+			if (requestType.equals("actuator")) {
+				requester.route("hello")
+						.metadata(client.forwarding(fwd -> fwd.serviceName("gateway")
+								.disableProxy()))
+						.data("ping")
+						.retrieveMono(String.class)
+						.subscribe(s -> log.info("received from actuator: " + s));
+			}
+			else if (requestType.equals("request-response")) {
 				Flux.interval(Duration.ofSeconds(1))
-					.flatMap(i -> requester.route("pong-rr")
-							.metadata(forwarding, Forwarding.FORWARDING_MIME_TYPE)
-							.data("ping" + i)
-							.retrieveMono(String.class)
-							.doOnNext(this::logPongs))
-					.subscribe();
+						.flatMap(i -> requester.route("pong-rr")
+								.metadata(client.forwarding("pong"))
+								.data("ping" + i)
+								.retrieveMono(String.class)
+								.doOnNext(this::logPongs))
+						.subscribe();
 
-			} else {
+			}
+			else {
 				requester.route("pong-rc")
-						.metadata(forwarding, Forwarding.FORWARDING_MIME_TYPE)
+						.metadata(client.forwarding("pong"))
 						.data(Flux.interval(Duration.ofSeconds(1)).map(this::getPayload)
 								.onBackpressureDrop(payload -> log
 										.info("Backpressure applied, dropping payload " + payload)))
 						.retrieveFlux(String.class)
 						.subscribe(this::logPongs);
 			}
+
 		}
 
 		private String getPayload(long i) {
@@ -120,7 +83,7 @@ public class PingApplication {
 
 		private void logPongs(String payload) {
 			int received = pongsReceived.incrementAndGet();
-			log.info("received " + payload + "(" + received + ") in Ping" + id);
+			log.info("received " + payload + "(" + received + ") in Ping" + client.getProperties().getRouteId());
 		}
 	}
 }
--- pong/src/main/java/org/springframework/cloud/rsocket/sample/pong/PongApplication.java
+++ pong/src/main/java/org/springframework/cloud/rsocket/sample/pong/PongApplication.java
@@ -1,49 +1,25 @@
 package org.springframework.cloud.rsocket.sample.pong;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Function;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tag;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.ByteBufUtil;
-import io.rsocket.ConnectionSetupPayload;
-import io.rsocket.Payload;
-import io.rsocket.RSocket;
-import io.rsocket.RSocketFactory;
-import io.rsocket.SocketAcceptor;
-import io.rsocket.micrometer.MicrometerRSocketInterceptor;
-import io.rsocket.transport.netty.server.TcpServerTransport;
-import io.rsocket.util.DefaultPayload;
-import io.rsocket.util.RSocketProxy;
 import lombok.extern.slf4j.Slf4j;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
-import org.springframework.cloud.gateway.rsocket.autoconfigure.GatewayRSocketAutoConfiguration;
-import org.springframework.cloud.gateway.rsocket.support.Forwarding;
-import org.springframework.cloud.gateway.rsocket.support.RouteSetup;
-import org.springframework.cloud.gateway.rsocket.support.TagsMetadata;
+import org.springframework.cloud.gateway.rsocket.client.BrokerClient;
+import org.springframework.cloud.gateway.rsocket.client.GatewayRSocketClientAutoConfiguration;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.RSocketStrategies;
-import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
 import org.springframework.stereotype.Controller;
 
-import static io.netty.buffer.Unpooled.EMPTY_BUFFER;
-import static org.springframework.cloud.gateway.rsocket.support.WellKnownKey.INSTANCE_NAME;
-
 @SpringBootApplication
+@Import(GatewayRSocketClientAutoConfiguration.class)
 public class PongApplication {
 
 	public static void main(String[] args) {
@@ -51,22 +27,8 @@ public class PongApplication {
 	}
 
 	@Bean
-	//TODO: client module?
-	public RSocketStrategiesCustomizer gatewayRSocketStrategiesCustomizer() {
-		return strategies -> {
-			strategies.decoder(new Forwarding.Decoder(), new RouteSetup.Decoder())
-					.encoder(new Forwarding.Encoder(), new RouteSetup.Encoder());
-		};
-	}
-
-	@Bean
-	public Pong pong(Environment env, MeterRegistry meterRegistry,
-			RSocketRequester.Builder requesterBuilder, RSocketStrategies strategies,
-			RSocketMessageHandler handler) {
-		//TODO: client module
-		GatewayRSocketAutoConfiguration.registerMimeTypes(strategies);
-		requesterBuilder.rsocketFactory(rsocketFactory -> rsocketFactory.acceptor(handler.responder()));
-		return new Pong(env, meterRegistry, requesterBuilder);
+	public Pong pong(BrokerClient brokerClient) {
+		return new Pong(brokerClient);
 	}
 
 	static String reply(String in) {
@@ -82,21 +44,12 @@ public class PongApplication {
 	}
 
 	@Slf4j
-	public static class Pong implements ApplicationListener<ApplicationReadyEvent>,
-			SocketAcceptor, Function<RSocket, RSocket> {
+	public static class Pong implements ApplicationListener<ApplicationReadyEvent> {
 
-		private final String id;
+		private final BrokerClient brokerClient;
 
-		private final MeterRegistry meterRegistry;
-
-		private final RSocketRequester.Builder requesterBuilder;
-
-		private final AtomicInteger pingsReceived = new AtomicInteger();
-
-		public Pong(Environment env, MeterRegistry meterRegistry, RSocketRequester.Builder requesterBuilder) {
-			this.id = env.getProperty("pong.id", "3");
-			this.meterRegistry = meterRegistry;
-			this.requesterBuilder = requesterBuilder;
+		public Pong(BrokerClient brokerClient) {
+			this.brokerClient = brokerClient;
 		}
 
 		@Override
@@ -107,20 +60,9 @@ public class PongApplication {
 			Boolean isClient = env.getProperty("pong.client", Boolean.class, true);
 
 			log.info("Starting Pong isClient: " + isClient);
-			Integer port = env.getProperty("spring.cloud.gateway.rsocket.server.port",
-					Integer.class, 7002);
-			MicrometerRSocketInterceptor interceptor = new MicrometerRSocketInterceptor(meterRegistry, Tag
-					.of("component", "pong"));
-			//ByteBuf announcementMetadata = Metadata.from("pong").with("id", "pong" + id).encode();
 
 			if (isClient) {
-				TagsMetadata tagsMetadata = TagsMetadata.builder()
-						.with(INSTANCE_NAME, "pong" + id)
-						.build();
-				RouteSetup routeSetup = new RouteSetup(new Long(id), "pong", tagsMetadata.getTags());
-				requesterBuilder.setupMetadata(routeSetup, RouteSetup.ROUTE_SETUP_MIME_TYPE)
-						.connectTcp("localhost", port)
-						.block();
+				brokerClient.connect().block();
 				/*RSocketFactory.connect()
 						.metadataMimeType(Metadata.ROUTING_MIME_TYPE)
 						.setupPayload(DefaultPayload
@@ -131,52 +73,13 @@ public class PongApplication {
 						.start()
 						.block();*/
 			} else { // start server
-				RSocketFactory.receive()
+				/*RSocketFactory.receive()
 						.addServerPlugin(interceptor)
 						.acceptor(this)
 						.transport(TcpServerTransport.create(port)) // listen on port
 						.start()
-						.subscribe();
+						.subscribe();*/
 			}
-		}
-
-		@Override
-		public Mono<RSocket> accept(ConnectionSetupPayload setup, RSocket sendingSocket) {
-			return Mono.just(apply(sendingSocket));
-		}
-
-		@Override
-		public RSocket apply(RSocket rSocket) {
-			return new RSocketProxy(rSocket) {
-
-				@Override
-				public Mono<Payload> requestResponse(Payload payload) {
-					return Mono.just(payload)
-							.map(Payload::getDataUtf8)
-							.doOnNext(this::logPings)
-							.map(PongApplication::reply)
-							.map(this::toPayload);
-				}
-
-				@Override
-				public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
-					return Flux.from(payloads)
-							.map(Payload::getDataUtf8)
-							.doOnNext(this::logPings)
-							.map(PongApplication::reply)
-							.map(this::toPayload);
-				}
-
-				private void logPings(String str) {
-					int received = pingsReceived.incrementAndGet();
-					log.info("received " + str + "("+received+") in Pong");
-				}
-
-				private Payload toPayload(String reply) {
-					ByteBuf data = ByteBufUtil.writeUtf8(ByteBufAllocator.DEFAULT, reply);
-					return DefaultPayload.create(data, EMPTY_BUFFER);
-				}
-			};
 		}
 	}
 
