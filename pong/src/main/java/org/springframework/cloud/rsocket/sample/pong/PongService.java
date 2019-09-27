21a3514209feeba6787182c1bde561f7bd5c6d2c
Spencer Gibb <spencer@gibb.us>
2019-09-27 13:00:52 -0400

    Adds `@EventListener` example

--- pong/src/main/java/org/springframework/cloud/rsocket/sample/pong/PongService.java
+++ pong/src/main/java/org/springframework/cloud/rsocket/sample/pong/PongService.java
@@ -4,14 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.cloud.gateway.rsocket.client.BrokerClient;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.PayloadApplicationEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PongService implements ApplicationListener<PayloadApplicationEvent<RSocketRequester>> {
+public class PongService {
 
 	private static final Logger log = LoggerFactory.getLogger(PongService.class);
 
@@ -23,9 +22,8 @@ public class PongService implements ApplicationListener<PayloadApplicationEvent<
 		this.env = env;
 	}
 
-	@Override
-	@SuppressWarnings("Duplicates")
-	public void onApplicationEvent(PayloadApplicationEvent<RSocketRequester> event) {
+	@EventListener
+	public void onRSocketRequester(RSocketRequester requester) {
 		Boolean isClient = env.getProperty("pong.client", Boolean.class, true);
 
 		log.info("Starting Pong isClient: " + isClient);
